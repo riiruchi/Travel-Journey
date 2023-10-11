36af2a51f626014d1b3b031e827590274905cc5d
riiruchi <ruchirasinha0@gmail.com>
2023-10-11 22:55:48 +0530

    Sign In UI Design

--- Travel Journey/Controllers/Auth/SignInViewController.swift
+++ Travel Journey/Controllers/Auth/SignInViewController.swift
@@ -9,17 +9,82 @@ import UIKit
 
 class SignInViewController: UITabBarController {
 
+    // Header View
+    private let headerView = SignInHeaderView()
+    
+    //Email field
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.keyboardType = .emailAddress
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        field.leftViewMode = .always
+        field.placeholder = "Email Address"
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = 8
+        field.layer.masksToBounds = true
+        return field
+    } ()
+    
+    //Password field
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        field.leftViewMode = .always
+        field.placeholder = "Password"
+        field.isSecureTextEntry = true
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = 8
+        field.layer.masksToBounds = true
+        return field
+    } ()
+    
+    //Sign In button
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setTitle("Sign In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    } ()
+    
+    //Create Account
+    private let createAccountButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create Account", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        return button
+    } ()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Sign In"
         view.backgroundColor = .systemBackground
+        view.addSubview(headerView)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(signInButton)
+        view.addSubview(createAccountButton)
         
-        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
-            if !IAPManager.shared.isPremium() {
-                let vc = PayWallViewController()
-                let navVC = UINavigationController(rootViewController: vc)
-                self.present(navVC, animated: true, completion: nil)
-            }
-        }
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        createAccountButton.addTarget(self, action: #selector(didTapCreateAccount), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        headerView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: view.height/5)
+        emailField.frame = CGRect(x: 20, y: headerView.bottom, width: view.width-40, height: 50)
+        passwordField.frame = CGRect(x: 20, y: emailField.bottom+10, width: view.width-40, height: 50)
+        signInButton.frame = CGRect(x: 20, y: passwordField.bottom+10, width: view.width-40, height: 50)
+        createAccountButton.frame = CGRect(x: 20, y: signInButton.bottom+40, width: view.width-40, height: 50)
+    }
+    
+    @objc func didTapSignIn() {
+        
+    }
+    
+    @objc func didTapCreateAccount() {
+        
     }
 }
+
---
+++ Travel Journey/Views/SignInHeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  SignInHeaderView.swift
+//  Travel Journey
+//
+//  Created by Ruchira  on 11/10/23.
+//
+
+import UIKit
+
+class SignInHeaderView: UIView {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .systemPink
+        return imageView
+    } ()
+    
+    private let label: UILabel = {
+        let label = UILabel ()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.text = "Exploring millions of articles"
+        return label
+    } ()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        addSubview(imageView)
+        addSubview(label)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let size: CGFloat = width/4
+        imageView.frame = CGRect(x: (width-size)/2, y: 10, width: size, height: size)
+        label.frame = CGRect(x: 20, y: imageView.bottom+10, width: width-40, height: height-size-30)
+    }
+}
+
