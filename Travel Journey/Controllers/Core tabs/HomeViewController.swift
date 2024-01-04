867417522a6fc7775dd6d667fb0073fddc498848
riiruchi <ruchirasinha0@gmail.com>
2024-01-05 00:41:45 +0530

    Creat post and homeview features added

--- Travel Journey/Controllers/Core tabs/HomeViewController.swift
+++ Travel Journey/Controllers/Core tabs/HomeViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let composeButton : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "square.and.pencil",
+                                withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)),
+                        for: .normal)
+        button.layer.cornerRadius = 40
+        button.layer.shadowColor = UIColor.label.cgColor
+        button.layer.shadowOpacity = 0.4
+        button.layer.shadowRadius = 10
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(composeButton)
+        composeButton.addTarget(self, action: #selector(didTapCreate), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        composeButton.frame = CGRect(x: view.frame.width - 80 - 16,
+                                     y: view.frame.height - 80 - 16 - view.safeAreaInsets.bottom,
+                                     width: 80,
+                                     height: 80)
+    }
+    
+    @objc private func didTapCreate() {
+        let vc = CreateNewPostViewController ()
+        vc.title = "Create Post"
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
     }
 }
 
--- Travel Journey/Controllers/Posts/CreateNewPostViewController.swift
+++ Travel Journey/Controllers/Posts/CreateNewPostViewController.swift
@@ -9,21 +9,103 @@ import UIKit
 
 class CreateNewPostViewController: UITabBarController {
 
+    //Title field
+    private let titleField: UITextField = {
+        let field = UITextField()
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        field.leftViewMode = .always
+        field.placeholder = "Enter title..."
+        field.autocapitalizationType = .words
+        field.autocorrectionType = .yes
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.masksToBounds = true
+        return field
+    } ()
+    
+    //Image Header
+    private let headerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(systemName: "photo")
+        imageView.backgroundColor = .tertiarySystemBackground
+        return imageView
+    }()
+    
+    //TextView for Post
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .secondarySystemBackground
+        textView.isEditable = true
+        textView.font = .systemFont(ofSize: 28)
+        return textView
+    }()
+    
+    private var selectedHeaderImage: UIImage?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(titleField)
+        view.addSubview(headerImageView)
+        view.addSubview(textView)
+        configureButton()
+    }
+    
+    override func viewDidLayoutSubviews() {
+         super.viewDidLayoutSubviews()
+        
+        titleField.frame = CGRect(x: 10,
+                                  y: view.safeAreaInsets.top,
+                                  width: view.width-20, 
+                                  height: 50)
+        headerImageView.frame = CGRect(x: 0,
+                                       y: titleField.bottom+5,
+                                       width: view.width,
+                                       height: 160)
+        textView.frame = CGRect(x: 10,
+                                y: headerImageView.bottom+10,
+                                width: view.width-20,
+                                height: view.height-210-view.safeAreaInsets.top)
+    }
+    
+    private func configureButton() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .done,
+            target: self,
+            action: #selector(didTapCancel)
+        )
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Post",
+            style: .done,
+            target: self,
+            action: #selector(didTapPost)
+        )
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func didTapCancel(){
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @objc private func didTapPost(){
+        //Check data and post
+        guard let title = titleField.text,
+              let body = textView.text,
+              let headerImage = selectedHeaderImage,
+              !title.trimmingCharacters(in: .whitespaces).isEmpty,
+              !body.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+//        let post = BlogPost(identifier: <#T##String#>,
+//                            title: <#T##String#>,
+//                            timeStamp: <#T##TimeInterval#>,
+//                            headerImageUrl: <#T##URL?#>,
+//                            text: <#T##String#>
+//       )
+    }
 }
